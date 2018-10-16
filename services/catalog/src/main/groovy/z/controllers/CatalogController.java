dd5c5821650cd3bfd8994635c7f2c17fa7c29c9e
Guangyan Zhang <gavin.g.zhang@gmail.com>
2018-10-16 20:30:17 +1100

    skeleton includes a service and db

---
+++ services/catalog/src/main/groovy/z/controllers/CatalogController.java
@@ -0,0 +1,29 @@
+package z.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+public class CatalogController {
+    private final static Logger log = LoggerFactory.getLogger(CatalogController.class);
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public CatalogController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @GetMapping(path = "/hello", produces = APPLICATION_JSON_VALUE)
+    public String hello() {
+        String name = jdbcTemplate.queryForObject("select name from catalog", String.class);
+        log.info("returned from db: {}", name);
+        return name;
+    }
+}
