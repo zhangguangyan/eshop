b86d23aa315ab6f8937aed3cd37f471d505c65a5
Guangyan Zhang <gavin.g.zhang@gmail.com>
2018-10-20 09:06:16 +1100

    add ordering service to the whole application

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-./gradlew clean :api.gateway:web.bff.shopping:docker :services:catalog:docker
+./gradlew clean :api.gateway:web.bff.shopping:docker :services:catalog:docker :services:ordering:docker
 cd webSPA && ./build.sh
 cd .. && docker-compose -f docker-compose.yml up
